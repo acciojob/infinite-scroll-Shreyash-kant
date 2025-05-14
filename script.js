ae462bf47445ee61912c0b6d5f283c98b5299c19
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-05-14 15:15:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
-
+const list = document.getElementById("infi-list");
+let numberOfItems = 0;
+function updateList(items) {
+  for (let i = 1; i <= items; ++i) {
+    const listItem = document.createElement("li");
+    listItem.textContent = `Item ${++numberOfItems}`;
+    list.appendChild(listItem);
+  }
+}
+updateList(10);
+function renderNew(event) {
+  updateList(2);
+}
+list.addEventListener("scroll", (event) => {
+  const scrolledHeight = list.scrollTop;
+  const elementHeight = list.clientHeight;
+  const totalContentHeight = list.scrollHeight;
+  if (scrolledHeight + elementHeight >= totalContentHeight - 5) {
+    renderNew(event);
+  }
+});
